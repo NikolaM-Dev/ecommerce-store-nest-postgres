b669a4102a3e1721d35094394e4a69e417484755
David Merchan <juan1014166@gmail.com>
2021-10-09 12:42:55 -0500

    feat(orders): Add get products and get total

--- src/users/entities/order.entity.ts
+++ src/users/entities/order.entity.ts
@@ -7,6 +7,7 @@ import {
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
+import { Exclude, Expose } from 'class-transformer';
 
 import { Customer } from './customer.entity';
 import { OrderItem } from './order-product.entity';
@@ -16,6 +17,7 @@ export class Order {
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Exclude()
   @CreateDateColumn({
     name: 'created_at',
     type: 'timestamptz',
@@ -23,6 +25,7 @@ export class Order {
   })
   createAt: Date;
 
+  @Exclude()
   @UpdateDateColumn({
     name: 'updated_at',
     type: 'timestamptz',
@@ -34,6 +37,32 @@ export class Order {
   @JoinColumn({ name: 'customer_id' })
   customer: Customer;
 
+  @Exclude()
   @OneToMany(() => OrderItem, (ordenItem) => ordenItem.order)
-  items: OrderItem;
+  items: OrderItem[];
+
+  @Expose()
+  get products() {
+    if (!this.items) return [];
+
+    return this.items
+      .filter((item) => !!item)
+      .map((item) => ({
+        ...item.product,
+        quantity: item.quantity,
+      }));
+  }
+
+  @Expose()
+  get total() {
+    if (!this.items) return 0;
+
+    return this.items
+      .filter((item) => !!item)
+      .reduce((total, item) => {
+        const totalItem = item.product.price * item.quantity;
+
+        return total + totalItem;
+      }, 0);
+  }
 }
