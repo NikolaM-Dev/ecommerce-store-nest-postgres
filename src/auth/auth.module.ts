4c9cc8189056f2b622f27d37a52fd70077231820
David Merchan <juan1014166@gmail.com>
2021-10-13 17:16:01 -0500

    feat(auth): Add login endpoint

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,9 +5,11 @@ import { UsersModule } from '../users/users.module';
 
 import { AuthService } from './services/auth.service';
 import { LocalStrategy } from './strategies/local.strategy';
+import { AuthController } from './controllers/auth.controller';
 
 @Module({
   imports: [UsersModule, PassportModule],
   providers: [AuthService, LocalStrategy],
+  controllers: [AuthController],
 })
 export class AuthModule {}
