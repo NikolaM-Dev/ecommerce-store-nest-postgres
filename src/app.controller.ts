ce4c3ff35fe6a664a75ba6dd1ad485a11fa2f566
David Merchan <juan1014166@gmail.com>
2021-10-13 10:15:51 -0500

    refactor(auth): Change isPublic to a decorator with its constant

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,15 +2,22 @@ import { Controller, Get, UseGuards } from '@nestjs/common';
 
 import { ApiKeyGuard } from './auth/guards/api-key.guard';
 import { AppService } from './app.service';
+import { IsPublic } from './auth/decorators/is-public.decorator';
 
+@UseGuards(ApiKeyGuard)
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @UseGuards(ApiKeyGuard)
   @Get()
+  @IsPublic()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('test')
+  getTest(): string {
+    return 'test 👀';
   }
 
   @Get('tasks')
--- src/auth/guards/api-key.guard.ts
+++ src/auth/guards/api-key.guard.ts
@@ -7,19 +7,26 @@ import {
 } from '@nestjs/common';
 import { ConfigType } from '@nestjs/config';
 import { Observable } from 'rxjs';
+import { Reflector } from '@nestjs/core';
 import { Request } from 'express';
 
+import { IS_PUBLIC_KEY } from '../decorators/is-public.decorator';
 import config from '../../config';
 
 @Injectable()
 export class ApiKeyGuard implements CanActivate {
   constructor(
+    private readonly reflector: Reflector,
     @Inject(config.KEY)
     private readonly configService: ConfigType<typeof config>,
   ) {}
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
+    const isPublic = this.reflector.get(IS_PUBLIC_KEY, context.getHandler());
+
+    if (isPublic) return true;
+
     const request = context.switchToHttp().getRequest<Request>();
     const authHeader = request.header('Auth');
     const isAuth = authHeader === this.configService.apiKey;
